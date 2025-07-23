1826bbab7ebdb198ed3ee013c313043dc16b52d2
Liming <wooof21@gmail.com>
2025-07-23 17:23:54 -0400

    Add files via upload

---
+++ src/main/java/com/example/kafka/stream/constant/MSOConstants.java
@@ -0,0 +1,12 @@
+package com.example.kafka.stream.constant;
+
+public class MSOConstants {
+
+    public static final String ET_TIMEZONE = "America/New_York";
+
+    public static final String EVENT_TYPE_FRL_RECEIVED = "FRL Received";
+
+    public static final Integer ALERT_CODE_FRL_RECEIVED = 270;
+
+
+}
